8ea2cd204326ac5c835307bf1d819bdac0a0d93d
zordan <zekagorobey@gmail.com>
2022-06-02 13:57:51 +0300

    add clg in server.js

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const jsonServer = require('json-server');
+
 const app = jsonServer.create();
 const path = require('path');
 const express = require('express');
+
 const middlewares = jsonServer.defaults({
-  static: './db.json'
+  static: './db.json',
 });
 const router = jsonServer.router('db.json');
 const port = process.env.PORT || 3001;
 
-app.use(middlewares)
+app.use(middlewares);
 app.use('/db', middlewares, router);
 app.use(express.static(path.join(__dirname, 'build')));
 
+console.log(`process.env.API_KEY: ${process.env.API_KEY}`);
+
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port);
