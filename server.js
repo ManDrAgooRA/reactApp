a62bf2bad7450c109431153abae543fd77839f91
zordan <zekagorobey@gmail.com>
2022-06-03 12:08:54 +0300

    add process.env.DATABASE_URL

--- server.js
+++ server.js
@@ -14,10 +14,6 @@ app.use(middlewares);
 app.use('/db', middlewares, router);
 app.use(express.static(path.join(__dirname, 'build')));
 
-console.log(`process.env.DATABASE_URL: ${process.env.DATABASE_URL}`);
-console.log(
-  `process.env.DATABASE_URL.API_KEY: ${process.env.DATABASE_URL.API_KEY}`
-);
 console.log(`process.env.API_KEY: ${process.env.API_KEY}`);
 
 app.get('/*', function (req, res) {
