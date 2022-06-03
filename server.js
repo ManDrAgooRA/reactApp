a7fba800c4ca74965066057d3a311591f47b450c
zordan <zekagorobey@gmail.com>
2022-06-03 22:55:48 +0300

    add json-server-auth in server.js

--- server.js
+++ server.js
@@ -1,29 +1,5 @@
-// const jsonServer = require('json-server');
-
-// const app = jsonServer.create();
-// const path = require('path');
-// const express = require('express');
-
-// const middlewares = jsonServer.defaults({
-//   static: './db.json',
-// });
-// const router = jsonServer.router('db.json');
-// const port = process.env.PORT || 3004;
-
-// app.use(middlewares);
-// app.use('/db', middlewares, router);
-// app.use(express.static(path.join(__dirname, 'build')));
-
-// console.log(`process.env.API_KEY: ${process.env.API_KEY}`);
-
-// app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-
-// app.listen(port);
-
 const jsonServer = require('json-server');
-
+const auth = require('json-server-auth');
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults({
@@ -39,6 +15,7 @@ server.use(
   })
 );
 
+server.use(auth);
 server.use(router);
 server.listen(port, () => {
   console.log(`Server is work on port: ${port}`);
