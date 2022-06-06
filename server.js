7e4eb87e5fe6d741d9c4d820da86ddea83b0c1f4
zordan <zekagorobey@gmail.com>
2022-06-06 20:18:18 +0300

    another setting server.js

--- server.js
+++ server.js
@@ -1,37 +1,58 @@
+// const jsonServer = require('json-server');
+// const auth = require('json-server-auth');
+
+// const server = jsonServer.create();
+// const router = jsonServer.router('./db.json');
+
+// server.get('/echo', (req, res) => {
+//   res.jsonp(req.query);
+// });
+
+// server.use(jsonServer.bodyParser);
+// server.use((req, res, next) => {
+//   if (req.method === 'POST') {
+//     req.body.createdAt = Date.now();
+//   }
+//   // Continue to JSON Server router
+//   next();
+// });
+
+// const middlewares = jsonServer.defaults({
+//   static: './build',
+// });
+
+// const port = process.env.PORT || 5000;
+
+// server.db = router.db;
+
+// server.use(middlewares);
+// server.use(
+//   jsonServer.rewriter({
+//     '/api/+': '/$1',
+//   })
+// );
+
+// server.use(auth);
+// server.use(router);
+// server.listen(port);
+
 const jsonServer = require('json-server');
-const auth = require('json-server-auth');
 
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
-
-server.get('/echo', (req, res) => {
-  res.jsonp(req.query);
-});
-
-server.use(jsonServer.bodyParser);
-server.use((req, res, next) => {
-  if (req.method === 'POST') {
-    req.body.createdAt = Date.now();
-  }
-  // Continue to JSON Server router
-  next();
-});
-
 const middlewares = jsonServer.defaults({
   static: './build',
 });
-
-const port = process.env.PORT || 5000;
-
-server.db = router.db;
-
+const PORT = process.env.PORT || 8000;
 server.use(middlewares);
 server.use(
   jsonServer.rewriter({
-    '/api/+': '/$1',
+    '/api/*': '/$1',
   })
 );
-
-server.use(auth);
 server.use(router);
-server.listen(port);
+server.listen(PORT, () => {
+  console.log('Server is running');
+});
+
+// server.use(auth);
