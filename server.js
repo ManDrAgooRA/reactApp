e5327c21ad1dbf1f273c72cb714c2f8327a6bc9f
zordan <zekagorobey@gmail.com>
2022-06-06 19:38:00 +0300

    change server.js

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const jsonServer = require('json-server');
 const auth = require('json-server-auth');
+const path = require('path');
 
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
+
+const routes = jsonServer.defaults({
+  static: path.join(__dirname, 'build'),
+});
+
 const middlewares = jsonServer.defaults({
   static: './build',
 });
+
 const port = process.env.PORT || 5000;
 
 server.db = router.db;
@@ -17,6 +24,11 @@ server.use(
   })
 );
 
+server.get((req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 server.use(auth);
 server.use(router);
 server.listen(port);
+server.use(routes);
