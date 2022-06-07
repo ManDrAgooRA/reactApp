cfb3188adab4ad920b86e059945086ea212d5b41
zordan <zekagorobey@gmail.com>
2022-06-07 11:22:29 +0300

    add server static

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
 const jsonServer = require('json-server');
 const auth = require('json-server-auth');
-// const path = require('path');
+const path = require('path');
 
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
 
-server.get('/echo', (req, res) => {
-  res.jsonp(req.query);
-});
-
-server.use(jsonServer.bodyParser);
-server.use((req, res, next) => {
-  if (req.method === 'POST') {
-    req.body.createdAt = Date.now();
-  }
-  // Continue to JSON Server router
-  next();
-});
+server.use(
+  jsonServer.defaults({
+    static: path.join(__dirname, './build'),
+    logger: false,
+  })
+);
 
 const middlewares = jsonServer.defaults({
   static: './build',
@@ -36,27 +30,3 @@ server.use(
 server.use(auth);
 server.use(router);
 server.listen(port);
-
-// const jsonServer = require('json-server');
-
-// const server = jsonServer.create();
-// const router = jsonServer.router('./db.json');
-// const middlewares = jsonServer.defaults({
-//   static: './build',
-// });
-
-// server.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, './build/index.html'));
-// });
-
-// const PORT = process.env.PORT || 8000;
-// server.use(middlewares);
-// server.use(
-//   jsonServer.rewriter({
-//     '/api/*': '/$1',
-//   })
-// );
-// server.use(router);
-// server.listen(PORT);
-
-server.use(auth);
